a0a3983379d6497ed899142b06c12c16587ede95
Ahmed Matem <ahmedmatem@gmail.com>
2026-01-09 15:18:16 +0200

    Create task component

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,14 +2,24 @@ import { Component } from '@angular/core';
 import { Header } from "./header/header";
 import { User } from "./user/user/user";
 import { DUMMY_USERS } from "./dummy-users";
+import { Task } from './task/task';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, User],
+  imports: [Header, User, Task],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   users = DUMMY_USERS;
   protected readonly title = 'first-app';
+  selectdeUserId: string = 'u1';
+  
+  get selectedUser(){
+    return this.users.find(u => u.id === this.selectdeUserId);
+  }
+
+  onSelectedUser(userId: string) {
+    this.selectdeUserId = userId;
+  }
 }
--- src/app/user/user/user.ts
+++ src/app/user/user/user.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -8,12 +8,14 @@ import { Component, Input, Output } from '@angular/core';
 })
 export class User {
   @Input({required: true}) user!: {id: string, name: string, avatar: string};
+  @Output() selectedUser = new EventEmitter<string>();
 
   get imagePath(){
     return 'assets/img/' + this.user.avatar;
   }
 
   onSelectedUser(){
-    console.log(`User ${this.user.name} with ID ${this.user.id} selected.`);
+    // console.log(`User ${this.user.name} with ID ${this.user.id} selected.`);
+    this.selectedUser.emit(this.user.id);
   }
 }
